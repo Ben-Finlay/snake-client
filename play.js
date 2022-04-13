a9eea7809b7b38e82794586857f9c6d816cc9da0
Ben Finlay <benjamin.finlay@gmail.com>
2022-04-13 15:03:51 -0400

    added two functions to handle movement and user input

--- play.js
+++ play.js
@@ -1,16 +1,25 @@
-const net = require("net");
+
 const connect = require("./client")
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
+
+const handleUserInput = function () {
+  if (key === 'q') {
+    process.exit();
+  }
+};
+
+
+setupInput();
 // establishes a connection with the game server
-
-
-
 console.log("Connecting ...");
 connect();
-
-/*  conn.on("data", (data) => {
-    // code that does something when the connection is first established
-    console.log(data);
-  });
-
-  */
